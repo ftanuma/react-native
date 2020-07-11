080c43a5295d55e3d15dbcc67e7b5c7534d381ed
Fumihiko Tanuma <ftanuma@gmail.com>
2020-07-10 22:42:10 -0700

    Add patch script

---
+++ RNTester/fabric-patch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp Pods/Headers/Private/Yoga/yoga/YGNode.h Pods/Headers/Public/Yoga/yoga/YGNode.h
+cp Pods/Headers/Private/Yoga/yoga/YGStyle.h Pods/Headers/Public/Yoga/yoga/YGStyle.h
+touch Pods/Headers/Public/React-Fabric/FBRCTFabricComponentsPlugins.h
